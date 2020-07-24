78d540571e583e9e6c0ef6c019ccc7051924827e
AniaCuapio <ania.cuapio@gmail.com>
2020-07-23 20:34:19 -0500

    post a la api agregado

--- js/newPost.js
+++ js/newPost.js
@@ -4,8 +4,24 @@ $("#buttonS").click(function () {
   const postObject = {};
   newProfile.map(function (value) {
     postObject[value.name] = value.value;
- }); 
-$.post("https://ajaxclass-1ca34.firebaseio.com/medium-equipo1/posts/.json", JSON.stringify(postObject), function (data){console.log (data)})
+  });
 
+  $.ajax({
+    url: "http://localhost:3000/entries",
+    method: "POST",
+    dataType: "json",
+    headers: {
+      Authorization:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMTlkMWFkNzYyODBkM2I2ODA1NTcyYyIsImlhdCI6MTU5NTUzMzE4NX0.ZbyLJGs_8UfaH68xixqkf-tn86sNpRqcYLlYDHr6PFE",
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify(postObject),
+    success: (response) => {
+      jQuery.parseJSON(response);
+      console.log(response);
+      console.log("registro exitoso");
+      getPostDb();
+    },
+  });
   $("#confirmModal").modal("show");
-});    
+});
